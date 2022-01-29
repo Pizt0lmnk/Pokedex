ea137527287e039abdb7afa11234cc53b3f212d8
DennisLudmann <dennisludmann@gmail.com>
2022-01-29 20:22:33 +0100

    img, headline-js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 
-
-
+let currentPokemon;
 
 async function loadPokemon(){
 
 let url = 'https://pokeapi.co/api/v2/pokemon/geodude';
 let response = await fetch(url);
-let responseAsJson = await response.json();
 
-console.log('Pokemon loaded:', responseAsJson);
+currentPokemon = await response.json();
+
+console.log('Pokemon loaded:', currentPokemon);
+renderPokemonInfo();
+}
+
+function renderPokemonInfo(){
+    
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonImage').src = currentPokemon["sprites"]["front_default"];
 }
