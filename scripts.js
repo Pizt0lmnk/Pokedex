0eff4d839657cbd0811a7cc34a2f1a27883a4814
DennisLudmann <dennisludmann@gmail.com>
2022-01-29 21:31:58 +0100

    basic-stats, background img - not styled

--- scripts.js
+++ scripts.js
@@ -15,5 +15,11 @@ renderPokemonInfo();
 function renderPokemonInfo(){
     
     document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonImage').src = currentPokemon["sprites"]["front_default"];
+    document.getElementById('pokemonImage').src = currentPokemon["sprites"]["other"]["dream_world"]["front_default"];
+    document.getElementById('skill-hp').innerHTML = currentPokemon["stats"][0]["base_stat"]+` hp`;
+    document.getElementById('skill-attack').innerHTML = currentPokemon["stats"][1]["base_stat"]+` attack`;
+    document.getElementById('skill-defence').innerHTML = currentPokemon["stats"][2]["base_stat"]+` defence`;
+    document.getElementById('skill-speed').innerHTML = currentPokemon["stats"][5]["base_stat"]+` speed`;
+    document.getElementById('type-1').innerHTML =currentPokemon["types"]["0"]["type"]["name"];
+    document.getElementById('type-2').innerHTML =currentPokemon["types"]["1"]["type"]["name"];
 }
