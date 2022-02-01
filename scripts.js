2a66665da8c1110ad193f7705c187af90f8adf61
DennisLudmann <dennisludmann@gmail.com>
2022-02-01 12:55:57 +0100

    toUppercase(), img position & sice, hide/show-cycle

--- scripts.js
+++ scripts.js
@@ -7,33 +7,32 @@ let currentPokemon;             // globaly declader to be used later in all func
 async function displayOverview() {
     for (let i = 0; i < allPokemon.length; i++) {                       // load all pokemon from array
         const thisPokemon = allPokemon[i];
-        let url = 'https://pokeapi.co/api/v2/pokemon/' + thisPokemon.toLowerCase();
+        let url = 'https://pokeapi.co/api/v2/pokemon/' + thisPokemon.toLowerCase(); //lowercase so it fits the URL
 
-        let response = await fetch(url);
-        currentPokemon = await response.json();
-        
+        let response = await fetch(url);                    //waiting so the function doesnt continue without this input
+        currentPokemon = await response.json();             // to json so we have a file type we can work with
+
         let Types = currentPokemon["types"].length;
-        
-        if (Types > 1) {
+        if (Types > 1) {                                    // to display only if bot types are set
             document.getElementById('overview').innerHTML += `
-        <div onclick="loadPokemon(${currentPokemon['name'].toUpperCase()})" style="height:250px; width:250px; position:relative;" class=" ">
-        <img style="height:100px;" src="${currentPokemon["sprites"]["other"]["dream_world"]["front_default"]}" class="mt-3 card-img" alt="...">
-        <div id ="quick-fix"class="bg-stone card-img-overlay">
-          <h5 class="card-title">${currentPokemon['name']}</h5>
-          <p class="card-text"> ${currentPokemon["types"]["0"]["type"]["name"]}</p>
-          <p class="card-text">${currentPokemon["types"]["1"]["type"]["name"]}</p>
-        </div>
-        </div>
+            <div onclick="loadPokemon('${currentPokemon['name']}')" style="height:250px; width:250px; position:relative;" class=" ">
+            <img style="height:100px;" src="${currentPokemon["sprites"]["other"]["dream_world"]["front_default"]}" class="mt-3 card-img" alt="...">
+            <div id ="quick-fix"class="card-img-overlay">
+              <h5 class="card-title">${currentPokemon['name'].toUpperCase()}</h5>
+              <p class="card-text"> ${currentPokemon["types"]["0"]["type"]["name"]}</p>
+              <p class="card-text">${currentPokemon["types"]["1"]["type"]["name"]}</p>
+            </div>
+            </div>
          `;
-           
         }
+
         else {
             document.getElementById('overview').innerHTML += `
-        <div onclick="loadPokemon(${currentPokemon['name']})" style="height:250px; width:250px; position:relative;" class=" ">
-        <img style="height:100px;" src="${currentPokemon["sprites"]["other"]["dream_world"]["front_default"]}" class="mt-3 card-img" alt="...">
-        <div id ="quick-fix"class="bg-stone card-img-overlay">
-          <h5 class="card-title">${currentPokemon['name']}</h5>
-          <p class="card-text"> ${currentPokemon["types"]["0"]["type"]["name"]}</p>
+            <div onclick="loadPokemon('${currentPokemon['name']}')" style="height:250px; width:250px; position:relative;" class=" ">
+            <img style="height:100px;" src="${currentPokemon["sprites"]["other"]["dream_world"]["front_default"]}" class="mt-4 ms-2 card-img" alt="...">
+            <div id ="quick-fix"class="card-img-overlay">
+              <h5 class="card-title">${currentPokemon['name'].toUpperCase()}</h5>
+                <p class="card-text"> ${currentPokemon["types"]["0"]["type"]["name"]}</p>
        
         </div>
         </div>
@@ -42,6 +41,8 @@ async function displayOverview() {
         }
     }
 }
+
+/*
     function SeconType() {
         let Types = currentPokemon["types"].length;
         if (Types > 1) {
@@ -49,40 +50,42 @@ async function displayOverview() {
 
         }
     }
+*/
 
 
-    function showCard() {
-        document.getElementById('pokedex').classList.remove('d-none');
-        document.getElementById('overview').classList.add('d-none');
+function renderPokemonInfo() {                                                       // shows a variaty of info on selected pokemnon 
 
-
-
-    }
-
-
-    function renderPokemonInfo() {                                                       // shows a variaty of info on selected pokemnon 
-
-        document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-        document.getElementById('pokemonImage').src = currentPokemon["sprites"]["other"]["dream_world"]["front_default"];   //its the .img so src =
-        document.getElementById('skill-hp').innerHTML = currentPokemon["stats"][0]["base_stat"] + ` hp`;
-        document.getElementById('skill-attack').innerHTML = currentPokemon["stats"][1]["base_stat"] + ` attack`;
-        document.getElementById('skill-defence').innerHTML = currentPokemon["stats"][2]["base_stat"] + ` defence`;
-        document.getElementById('skill-speed').innerHTML = currentPokemon["stats"][5]["base_stat"] + ` speed`;
-        document.getElementById('type-1').innerHTML = currentPokemon["types"]["0"]["type"]["name"];
-        document.getElementById('type-2').innerHTML = currentPokemon["types"]["1"]["type"]["name"];
-    }
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'].toUpperCase();
+    document.getElementById('pokemonImage').src = currentPokemon["sprites"]["other"]["dream_world"]["front_default"];   //its the .img so src =
+    document.getElementById('skill-hp').innerHTML = currentPokemon["stats"][0]["base_stat"] + ` hp`;
+    document.getElementById('skill-attack').innerHTML = currentPokemon["stats"][1]["base_stat"] + ` attack`;
+    document.getElementById('skill-defence').innerHTML = currentPokemon["stats"][2]["base_stat"] + ` defence`;
+    document.getElementById('skill-speed').innerHTML = currentPokemon["stats"][5]["base_stat"] + ` speed`;
+    document.getElementById('type-1').innerHTML = currentPokemon["types"]["0"]["type"]["name"];
+    document.getElementById('type-2').innerHTML = currentPokemon["types"]["1"]["type"]["name"];
+}
 
 
 
-    async function loadPokemon(name) {
-        console.log('The picked Pokemon is', name);
-        debugger;
-        let url = 'https://pokeapi.co/api/v2/pokemon/' + name;        // to lowercase to make all characters small
-        let response = await fetch(url);                                                    // wait for the fetch
-        currentPokemon = await response.json();                                             //declare api/json to be used later
+async function loadPokemon(name) {
+    showCard();
+    console.log('The picked Pokemon is', name);
+    let url = 'https://pokeapi.co/api/v2/pokemon/' + name;        // to lowercase to make all characters small
+    let response = await fetch(url);                                                    // wait for the fetch
+    currentPokemon = await response.json();                                             //declare api/json to be used later
 
-        showCard();
-        renderPokemonInfo();                                                         // display single pokemoncard
-    }
+    renderPokemonInfo();                                                         // display single pokemoncard
+}
 
+
+function showCard() {
+    document.getElementById('pokedex').classList.remove('d-none');
+    document.getElementById('overview').classList.add('d-none');
+}
+
+
+function hideCard() {
+    document.getElementById('pokedex').classList.add('d-none');
+    document.getElementById('overview').classList.remove('d-none');
+}
 
